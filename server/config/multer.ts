79bbbc4f019146a5235e80dd3fe9baf5a6162613
Rafael Fischer <rafaelsanfischer@gmail.com>
2024-06-26 20:20:46 -0300

    feat: trying to create

--- server/config/multer.ts
+++ server/config/multer.ts
@@ -1,58 +1,17 @@
-// import multer from "multer";
-// import { NextApiRequest } from "next";
-
-// class FileLoader {
-//   private upload = multer({
-//     storage: multer.diskStorage({}),
-//     limits: { fileSize: 20 * 1024 * 1024 },
-//   });
-
-//   disableBodyParser = () => {
-//     return {
-//       api: {
-//         bodyParser: false,
-//       },
-//     };
-//   };
-
-//   uploadFileTemp = async (req: any, res: any) => {
-//     await new Promise((resolve, reject) => {
-//       this.upload.single("file")(req as any, res as any, (err: any) => {
-//         if (err instanceof multer.MulterError) {
-//           console.error(err);
-//           return reject({ status: 400, message: "File upload error" });
-//         }
-//         if (err) {
-//           console.error(err);
-//           return reject({ status: 500, message: "Internal server error" });
-//         }
-//         resolve("");
-//       });
-//     });
-//   };
-// }
-
-// export const fileLoader = new FileLoader();
-// export interface NextApiRequestWithFile extends NextApiRequest {
-//   file: {
-//     fieldname: string;
-//     originalname: string;
-//     encoding: string;
-//     mimetype: string;
-//     destination: string;
-//     filename: string;
-//     path: string;
-//     size: number;
-//   };
-// }
-
 import multer from "multer";
+import fs from "fs";
+import path from "path";
 import { NextApiRequest } from "next";
 
 class FileLoader {
   private upload = multer({
     storage: multer.diskStorage({
       destination: function (req, file, cb) {
+        const uploadDir = path.join(process.cwd(), "temp");
+
+        if (!fs.existsSync(uploadDir)) {
+          fs.mkdirSync(uploadDir, { recursive: true });
+        }
         cb(null, "./temp");
       },
       filename: function (req, file, cb) {
@@ -100,3 +59,51 @@ export const fileLoader = new FileLoader();
 export interface NextApiRequestWithFile extends NextApiRequest {
   file: any;
 }
+
+// import multer from "multer";
+// import { NextApiRequest } from "next";
+
+// class FileLoader {
+//   private upload = multer({
+//     storage: multer.diskStorage({}),
+//     limits: { fileSize: 20 * 1024 * 1024 },
+//   });
+
+//   disableBodyParser = () => {
+//     return {
+//       api: {
+//         bodyParser: false,
+//       },
+//     };
+//   };
+
+//   uploadFileTemp = async (req: any, res: any) => {
+//     await new Promise((resolve, reject) => {
+//       this.upload.single("file")(req as any, res as any, (err: any) => {
+//         if (err instanceof multer.MulterError) {
+//           console.error(err);
+//           return reject({ status: 400, message: "File upload error" });
+//         }
+//         if (err) {
+//           console.error(err);
+//           return reject({ status: 500, message: "Internal server error" });
+//         }
+//         resolve("");
+//       });
+//     });
+//   };
+// }
+
+// export const fileLoader = new FileLoader();
+// export interface NextApiRequestWithFile extends NextApiRequest {
+//   file: {
+//     fieldname: string;
+//     originalname: string;
+//     encoding: string;
+//     mimetype: string;
+//     destination: string;
+//     filename: string;
+//     path: string;
+//     size: number;
+//   };
+// }
