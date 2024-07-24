0393b0a352b202626bb7390903e27889507d9486
Rafael Fischer <rafaelsanfischer@gmail.com>
2024-07-24 18:18:47 -0300

    feat: ok

---
+++ modules/content-to-linkedin/backend/index.ts
@@ -0,0 +1,15 @@
+export const contentToLinkedin = {};
+
+interface ILinkedinTemplate {
+  id: string;
+  template: string;
+  tags: string[];
+  title: string;
+  description: string;
+}
+
+const templates: ILinkedinTemplate = [
+  {
+    id: "1",
+  },
+];
